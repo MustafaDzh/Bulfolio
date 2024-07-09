b0e6019dc3487b50ef9d51d6a309f277dcc0dd07
mustafadzhindzhi <dzindzimustafa@gmail.com>
2024-07-09 16:09:54 +0300

    portfolio

--- js/index.js
+++ js/index.js
@@ -1,14 +1,29 @@
-$(document).ready(function() {
+$(document).ready(function () {
+  var player = videojs('my-video');
 
-    var player = videojs('my-video');
+  $('.question-text p').hide();
 
-    $('.question-text p').hide();
+  $('.arrow-icon').click(function () {
+    $(this).toggleClass('rotated');
+    var question = $(this).closest('.question');
+    $(this).siblings('.question-text').children('p').slideToggle();
+    question.toggleClass('active');
+    $('.question').not(question).removeClass('active');
+  });
 
-    $('.arrow-icon').click(function() {
-      $(this).toggleClass('rotated');
-      var question = $(this).closest('.question');
-      $(this).siblings('.question-text').children('p').slideToggle();
-      question.toggleClass('active');
-      $('.question').not(question).removeClass('active');
+  var swiper = new Swiper('.car-swiper_container', {
+    loop: true,
+    slidesPerView: 1,
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+    },
+    keyboard: {
+      enabled: true,
+      onlyInViewport: true,
+    },
+    preloadImages: true,
+    updateOnImagesReady: true,
+    loopAdditionalSlides: 1,
   });
 });
