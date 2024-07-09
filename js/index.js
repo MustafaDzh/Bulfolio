f7fc1d8e283386516ffea7af82e1329ec51c3e0d
mustafadzhindzhi <dzindzimustafa@gmail.com>
2024-07-09 13:56:14 +0300

    questions

--- js/index.js
+++ js/index.js
@@ -1,6 +1,14 @@
-import $ from 'jquery';
-import videojs from 'video.js';
-
 $(document).ready(function() {
-  var player = videojs('my-video');
+
+    var player = videojs('my-video');
+
+    $('.question-text p').hide();
+
+    $('.arrow-icon').click(function() {
+      $(this).toggleClass('rotated');
+      var question = $(this).closest('.question');
+      $(this).siblings('.question-text').children('p').slideToggle();
+      question.toggleClass('active');
+      $('.question').not(question).removeClass('active');
+  });
 });
