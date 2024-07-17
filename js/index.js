d214c89d376495a3d7f2717366b32dff049457c6
mustafadzhindzhi <dzindzimustafa@gmail.com>
2024-07-17 14:19:59 +0300

    last changes

--- js/index.js
+++ js/index.js
@@ -1,4 +1,6 @@
 $(document).ready(function () {
+
+  //VIDEO PLAYER
   var player = videojs('my-video', {
     autoplay: true,
     muted: true, 
@@ -35,6 +37,8 @@ $(document).ready(function () {
     $playBtn.fadeIn();
   });
 
+
+  //QUESTION OPENING
   $('.question-text p').hide();
 
   $('.question').click(function() {
@@ -48,15 +52,16 @@ $(document).ready(function () {
   });
   
 
+  //PORTFOLIO SWIPER
   var swiper = new Swiper('.swiper-container', {
     loop: true,
     slidesPerView: 1,
-    spaceBetween: 10,
+    spaceBetween: 0,
     centeredSlides: true,
-    autoplay: {
-      delay: 3000,
-      disableOnInteraction: false,
-    },
+    // autoplay: {
+    //   delay: 3000,
+    //   disableOnInteraction: false,
+    // },
     keyboard: {
       enabled: true,
       onlyInViewport: true,
@@ -65,7 +70,9 @@ $(document).ready(function () {
     updateOnImagesReady: true,
     loopAdditionalSlides: 1,
   });
+  
 
+  //DROPDOWN MENU
   $('.links-list--item').hover(
     function() {
         var $dropdown = $(this).find('.dropdown-menu');
@@ -81,13 +88,127 @@ $(document).ready(function () {
     }
 );
 
+//OFFERS HOVER
+
 $('.offers-offer').hover(
   function() {
-    $(this).siblings('.offers-offer').addClass('inactive');
-    $(this).removeClass('inactive').addClass('active');
+    $(this).addClass('active').siblings('.offers-offer').addClass('inactive');
   },
   function() {
+    // Mouse leave: remove all classes
     $('.offers-offer').removeClass('inactive active');
   }
 );
+
+$('.swiper-slide').hover(
+  function() {
+      $('.portfolio-svg').css('transform', 'rotate(45deg)');
+  },
+  function() {
+      $('.portfolio-svg').css('transform', 'none');
+  }
+);
 });
+
+//FORM VALIDATION
+$(document).ready(function() {
+  $('.upload').on('click', function() {
+    $('#file-upload').click();
+});
+
+$('#file-upload').on('change', function(event) {
+    const file = event.target.files[0];
+    const reader = new FileReader();
+    reader.onload = function(e) {
+        $('#uploaded-image-container').html(`<img src="${e.target.result}" alt="Uploaded Image">`);
+    };
+    reader.readAsDataURL(file);
+});
+
+  $('#contact-form').submit(function(event) {
+    event.preventDefault();
+    if (validateForm()) {
+      $(this).trigger('reset'); 
+      resetValidationStyles(); 
+      $('#uploaded-image-container').html(''); 
+      $('#thank-you-message').show(); 
+    }
+  });
+
+  function validateForm() {
+    var isValid = true;
+    resetValidationStyles();
+
+    // Validate Name
+    if ($('#name').val().trim() === '') {
+      isValid = false;
+      $('#name').addClass('error');
+    } else {
+      $('#name').removeClass('error');
+    }
+
+    // Validate Email
+    var email = $('#email').val().trim();
+    if (email === '') {
+      isValid = false;
+      $('#email').addClass('error');
+    } else {
+      var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      if (!emailRegex.test(email)) {
+        isValid = false;
+        $('#email').addClass('error');
+      } else {
+        $('#email').removeClass('error');
+      }
+    }
+
+    // Validate Phone (optional)
+    var phone = $('#phone').val().trim();
+    var phoneRegex = /^[0-9]{10}$/;
+    if (!phoneRegex.test(phone)) {
+      isValid = false;
+      $('#phone').addClass('error');
+    } else {
+      $('#phone').removeClass('error');
+    }
+
+    var radioChecked = false;
+    $('input[name="radio-group"]').each(function() {
+      if ($(this).is(':checked')) {
+        radioChecked = true;
+      }
+    });
+    if (!radioChecked) {
+      isValid = false;
+      $('.inputs').addClass('error');
+    } else {
+      $('.inputs').removeClass('error');
+    }
+
+    // Validate Message
+   var message = $('#message').val().trim();
+  if (message === '') {
+    isValid = false;
+    $('#message').addClass('error');
+  } else {
+    $('#message').removeClass('error');
+  }
+
+    // Validate Checkbox (terms agreement)
+    if (!$('#agree_terms').is(':checked')) {
+      isValid = false;
+      $('#terms-error').show(); 
+    } else {
+      $('#terms-error').hide();
+    }
+
+    return isValid;
+  }
+
+  function resetValidationStyles() {
+    $('.form-control').removeClass('error');
+    $('.inputs').removeClass('error');
+    $('#terms-error').hide();
+  }
+});
+
