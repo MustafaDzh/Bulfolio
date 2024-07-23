65e6fc754d8b0f56a542216fecdf32bef80fcc41
mustafadzhindzhi <dzindzimustafa@gmail.com>
2024-07-23 16:19:15 +0300

    form responsive

--- js/index.js
+++ js/index.js
@@ -1,29 +1,29 @@
 $(document).ready(function () {
 
-  //MOBILE NAV
+  // MOBILE NAV
   const $hamburgerBtn = $('#check-icon');
-    const $mobileNav = $('.header-container--mobile');
-    const $bars = $('.bar');
+  const $mobileNav = $('.header-container--mobile');
+  const $bars = $('.bar');
 
-    function toggleMenu() {
-        if ($hamburgerBtn.is(':checked')) {
-            $mobileNav.addClass('open');
-            $bars.addClass('cross');
-            $('html').addClass('no-scroll');
-            $('body').addClass('no-scroll');
-        } else {
-            $mobileNav.removeClass('open');
-            $bars.removeClass('cross');
-            $('html').removeClass('no-scroll');
-            $('body').removeClass('no-scroll');
-        }
+  function toggleMenu() {
+    if ($hamburgerBtn.is(':checked')) {
+      $mobileNav.addClass('open');
+      $bars.addClass('cross');
+      $('html').addClass('no-scroll');
+      $('body').addClass('no-scroll');
+    } else {
+      $mobileNav.removeClass('open');
+      $bars.removeClass('cross');
+      $('html').removeClass('no-scroll');
+      $('body').removeClass('no-scroll');
     }
+  }
 
-    $hamburgerBtn.on('change', function() {
-        toggleMenu();
-    });  
+  $hamburgerBtn.on('change', function() {
+    toggleMenu();
+  });
 
-  //VIDEO PLAYER
+  // VIDEO PLAYER
   var player = videojs('my-video', {
     autoplay: true,
     muted: true, 
@@ -31,21 +31,20 @@ $(document).ready(function () {
     preload: 'auto'
   });
 
-  //QUESTION OPENING
-  $('.question-text p').hide();
+  // QUESTION OPENING
+  $('.question-text .answer').hide();
 
   $('.question').click(function() {
-      var $this = $(this);
-      var answer = $this.find('.question-text p');
-      var arrow = $this.find('.arrow-icon');
+    var $this = $(this);
+    var answer = $this.find('.question-text .answer');
+    var arrow = $this.find('.arrow-icon');
   
-      answer.slideToggle();
-      $this.toggleClass('active');
-      arrow.toggleClass('rotated');
+    answer.slideToggle();
+    $this.toggleClass('active');
+    arrow.toggleClass('rotated');
   });
   
-
-  //PORTFOLIO SWIPER
+  // PORTFOLIO SWIPER
   var swiper = new Swiper('.portfolio-container', {
     loop: true,
     slidesPerView: 3.34,
@@ -61,52 +60,51 @@ $(document).ready(function () {
     },
   });
 
-  //DROPDOWN MENU
+  // DROPDOWN MENU
   $('.links-list--item').hover(
     function() {
-        var $icon = $(this).find('.dropdown-icon');
-        $icon.addClass('rotated');
+      var $icon = $(this).find('.dropdown-icon');
+      $icon.addClass('rotated');
     },
     function() {
-        var $icon = $(this).find('.dropdown-icon');
-        $icon.removeClass('rotated');
+      var $icon = $(this).find('.dropdown-icon');
+      $icon.removeClass('rotated');
     }
-);
+  );
 
-//OFFERS HOVER
-// $('.offers-offer').hover(
-//   function() {
-//     $(this).addClass('active').siblings('.offers-offer').addClass('inactive');
-//   },
-//   function() {
-//     $('.offers-offer').removeClass('inactive active');
-//   }
-// );
+  // OFFERS HOVER for DESKTOP
+  // $('.offers-offer').hover(
+  //   function() {
+  //     $(this).addClass('active').siblings('.offers-offer').addClass('inactive');
+  //   },
+  //   function() {
+  //     $('.offers-offer').removeClass('inactive active');
+  //   }
+  // );
 
-$('.swiper-slide').hover(
-  function() {
+  // PORTFOLIO SWIPER SVG EFFECT
+  $('.swiper-slide').hover(
+    function() {
       $('.portfolio-svg').css('transform', 'rotate(45deg)');
-  },
-  function() {
+    },
+    function() {
       $('.portfolio-svg').css('transform', 'none');
-  }
-);
-});
+    }
+  );
 
-//FORM VALIDATION
-$(document).ready(function() {
+  // FORM VALIDATION
   $('.upload').on('click', function() {
     $('#file-upload').click();
-});
+  });
 
-$('#file-upload').on('change', function(event) {
+  $('#file-upload').on('change', function(event) {
     const file = event.target.files[0];
     const reader = new FileReader();
     reader.onload = function(e) {
-        $('#uploaded-image-container').html(`<img src="${e.target.result}" alt="Uploaded Image">`);
+      $('#uploaded-image-container').html(`<img src="${e.target.result}" alt="Uploaded Image">`);
     };
     reader.readAsDataURL(file);
-});
+  });
 
   $('#contact-form').submit(function(event) {
     event.preventDefault();
@@ -169,13 +167,13 @@ $('#file-upload').on('change', function(event) {
     }
 
     // Validate Message
-   var message = $('#message').val().trim();
-  if (message === '') {
-    isValid = false;
-    $('#message').addClass('error');
-  } else {
-    $('#message').removeClass('error');
-  }
+    var message = $('#message').val().trim();
+    if (message === '') {
+      isValid = false;
+      $('#message').addClass('error');
+    } else {
+      $('#message').removeClass('error');
+    }
 
     // Validate Checkbox (terms agreement)
     if (!$('#agree_terms').is(':checked')) {
@@ -194,73 +192,74 @@ $('#file-upload').on('change', function(event) {
     $('#terms-error').hide();
   }
 
+  // Show More Button for Defence Cars
   $('.defence-car_third .card .desc').each(function() {
     var $desc = $(this);
     var $showMoreButton = $('<div class="show-more">Покажи повече</div>');
     $desc.parent().append($showMoreButton);
 
     function updateButtonVisibility() {
-        var isOverflowing = $desc[0].scrollHeight > $desc[0].clientHeight;
-        $showMoreButton.toggle(isOverflowing);
+      var isOverflowing = $desc[0].scrollHeight > $desc[0].clientHeight;
+      $showMoreButton.toggle(isOverflowing);
     }
 
     updateButtonVisibility();
 
     $showMoreButton.on('click', function() {
-        if ($desc.hasClass('expanded')) {
-            $desc.removeClass('expanded');
-            $(this).text('Покажи повече');
-        } else {
-            $desc.addClass('expanded');
-            $(this).text('Покажи по-малко');
-        }
+      if ($desc.hasClass('expanded')) {
+        $desc.removeClass('expanded');
+        $(this).text('Покажи повече');
+      } else {
+        $desc.addClass('expanded');
+        $(this).text('Покажи по-малко');
+      }
     });
 
     $(window).on('resize', updateButtonVisibility);
-});
-
- //PORTFOLIO SWIPER
- var newSwiper = new Swiper('.offers-content_swiper', {
-  loop: false,
-  direction: 'horizontal',
-  slidesPerView: 1,
-  spaceBetween: 20,
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-    renderBullet: function (index, className) {
-      return '<span class="' + className + '"></span>';
-    },
-  },
-  centeredSlides: true, 
-  keyboard: {
-    enabled: true,
-    onlyInViewport: true,
-  },
-  observer: true,
-  observeParents: true,
-});
-
-//SHOWING BUTTON ON MOBILE DEVICES
-function handleButtonClick() {
-  $('.defence-car_title-btn').each(function() {
-      $(this).off('click').on('click', function() {
-          $('.defence-car_title-btn').not(this).removeClass('active');
-          $(this).toggleClass('active');
-      });
   });
-}
 
-function updateEventHandlers() {
-  if (window.innerWidth <= 1024) {
-      handleButtonClick();
-  } else {
-      $('.defence-car_title-btn').off('click');
+  // OFFERS SWIPER
+  var newSwiper = new Swiper('.offers-content_swiper', {
+    loop: false,
+    direction: 'horizontal',
+    slidesPerView: 1,
+    spaceBetween: 20,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      renderBullet: function (index, className) {
+        return '<span class="' + className + '"></span>';
+      },
+    },
+    centeredSlides: true, 
+    keyboard: {
+      enabled: true,
+      onlyInViewport: true,
+    },
+    observer: true,
+    observeParents: true,
+  });
+
+  // MOBILE INTERACTIONS
+  function handleOffersClick() {
+    $('.offers-offer').off('click').on('click', function() {
+      var $this = $(this);
+
+      // On mobile, toggle the active class
+      $('.offers-offer').not($this).removeClass('active');
+      $this.toggleClass('active');
+    });
   }
-}
 
-updateEventHandlers();
-$(window).on('resize', updateEventHandlers);
+  function updateEventHandlers() {
+    if (window.innerWidth <= 1024) {
+      handleOffersClick();
+    } else {
+      $('.offers-offer').off('click');
+    }
+  }
+
+  updateEventHandlers();
+  $(window).on('resize', updateEventHandlers);
 
 });
-
